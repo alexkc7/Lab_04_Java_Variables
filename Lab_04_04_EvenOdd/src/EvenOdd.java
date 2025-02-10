abaf85ff1365378ad0445b638e2c6d1dd35a872a
alex cope <alexandercope05@gmail.com>
2025-02-09 20:06:59 -0500

    1090C Lab 4 Task 4
    Even/Odd Checker

---
+++ Lab_04_04_EvenOdd/src/EvenOdd.java
@@ -0,0 +1,12 @@
+public class EvenOdd {
+    public static void main(String[] args)
+    {
+        int numToExamine = 2;
+
+        if (numToExamine % 2 == 0) {
+            System.out.println(numToExamine + " is an even number.");
+    } else {
+            System.out.println(numToExamine + "is an odd number.");
+        }
+    }
+}
